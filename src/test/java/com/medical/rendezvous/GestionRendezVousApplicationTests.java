c406d9146555f15b29102adc59b72d109dfff40b
Kuns Lalaina <kuns665.kunslalaina665@gmail.com>
2026-09-12 16:18:01 +0300

    Separation de CI et CD

--- src/test/java/com/medical/rendezvous/GestionRendezVousApplicationTests.java
+++ src/test/java/com/medical/rendezvous/GestionRendezVousApplicationTests.java
@@ -2,13 +2,13 @@ package com.medical.rendezvous;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class GestionRendezVousApplicationTests {
 
     @Test
     void contextLoads() {
-    	assertEquals(5, 2 + 2);
+    	
     }
 }
